f6e10195d7c78edb62f68682f9c6a30ca065e4f8
Andre Arko <andre@arko.net>
2020-03-15 15:07:02 -0700

    [tools] give every post a postfiles array

--- _plugins/postfiles.rb
+++ _plugins/postfiles.rb
@@ -24,9 +24,13 @@ module Jekyll
         # Get the directory that files from this post would be in
         postfile_dir = File.join(site.config['source'], '_postfiles', postfile_id)
 
+        post.data["postfiles"] ||= []
+
         # Add a static file entry for each postfile, if any
-        Dir[File.join(postfile_dir, '/*')].each do |pf|
-          site.static_files << PostFile.new(site, postfile_dir, post.url, File.basename(pf))
+        Dir[File.join(postfile_dir, '/*')].sort.each do |pf|
+          postfile = PostFile.new(site, postfile_dir, post.url, File.basename(pf))
+          site.static_files << postfile
+          post.data["postfiles"] << postfile
         end
       end
     end
